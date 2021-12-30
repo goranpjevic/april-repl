970d63891bf5b7fa63d470c77515e85524137b4c
Goran Pjević <67331732+goranpjevic@users.noreply.github.com>
2021-12-30 07:33:55 +0100

    add comment and switch two lines
    a comment explaining the ')exit' system command has been added.  the
    lines to check for both system commands are now next to each other, to
    improve the readability of the code.

--- april-repl.lisp
+++ april-repl.lisp
@@ -7,13 +7,14 @@
     (princ "> ")
     (finish-output)
     (let ((str (read-line)))
-      (cond ((string-equal str ")exit") (return))
-	    ((string-equal str "") ())
+      (cond ((string-equal str "") ())
+	    ; exit repl when user inputs ')exit'
+	    ((string-equal str ")exit") (return))
 	    ; load apl file when user inputs ')load'
-            ((string-equal str ")load")
+	    ((string-equal str ")load")
 	     (progn
 	       (princ "load apl file: ")
 	       (finish-output)
 	       (eval '(april:april-load (pathname (read-line))))))
-            (t (april:april-f str))))
-  (terpri)))
+	    (t (april:april-f str))))
+    (terpri)))
